73df4736ee6113e7f4172c5a258cddc56f9fbc33
Hany-Kim <hanykim1210@gmail.com>
2025-08-25 08:01:12 +0900

    상수 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-// camel 표기볍을 통한 가독성 향상
-let userInfo;
-let isTrueOrFalse;
+// 변수 : 변하는 값을 저장하는 저장소
+let phone1 = "samsung";
+phone1 = "apple";
+console.log(phone1);
+
+// 상수 : 변하지 않는 값을 저장하는 저장소
+const phone2 = "samsung";
+phone2 = "apple"; // 오류
+console.log(phone2);
