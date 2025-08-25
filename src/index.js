57e55225390f2099dfddc28892ebffe61a4008b0
Hany-Kim <hanykim1210@gmail.com>
2025-08-25 09:12:58 +0900

    자료형과 형변환 7 - Null

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-// 원시 타입 자료형 - boolean
-// true / false
+// 원시 타입 자료형 - Null
+// 존재 X, 알 수 없는 값
 
-let isClicked = false;
+// 다른 언어에서 Null은 하나의 값으로 활용
+// JS는 오직 Null값만을 포함하는 '자료형'
 
-if (isClicked) {
-  console.log("클릭O");
-} else {
-  console.log("클릭X");
-}
+let num = null;
+console.log(typeof num); // JS의 오래된 오류 | 출력 : object
+console.log(num === null); // === : 비교 연산자 | 출력: true
