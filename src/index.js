f04a4b8182d84e06c004df3c7d2c20c85b86ef93
Hany-Kim <hanykim1210@gmail.com>
2025-08-25 09:00:51 +0900

    자료형과 형변환 3 - 숫자형

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
-// typeof : 특정 변수의 자료형을 결과값으로 반환
-let num = 10;
+// 원시 타입 자료형 - 숫자형
 
+let num1 = 125;
+let num2 = 10.00012;
+
+console.log(typeof num1); // number
+console.log(typeof num2); // number
+
+// Infinity
+let num = Infinity; // 무한대
 console.log(typeof num); // number
+
+let inf = 100 / 0; // 어떤 수에 0을 나누면 무한대
+console.log(inf); // Infinity
+
+// NaN : Not a Number
+let number = NaN;
+console.log(typeof number); // number
+
+let nan = "javascript" / 10;
+console.log(nan); // NaN
+
+// 잘못된 연산이라도 Infinity와 NaN으로 표시되기 때문에, JS의 숫자연산은 '안전하다'
