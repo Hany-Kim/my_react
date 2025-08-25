2170eb02b4d89274d94c56c720350f289baefb77
Hany-Kim <hanykim1210@gmail.com>
2025-08-25 09:08:41 +0900

    자료형과 형변환 6 - boolean

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-// 원시 타입 자료형 - string
+// 원시 타입 자료형 - boolean
+// true / false
 
-let name = "hans";
-console.log(typeof name); // string
+let isClicked = false;
 
-// 템플릿 리터럴 - 백틱을 사용하면 ${}을 사용 가능
-let intro = `제 이름은 ${name} 입니다.`;
-
-console.log(intro); // 제 이름은 hans 입니다.
+if (isClicked) {
+  console.log("클릭O");
+} else {
+  console.log("클릭X");
+}
