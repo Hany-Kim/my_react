678c58fb3545c6c1716825beafd18f130ef36334
Hany-Kim <hanykim1210@gmail.com>
2025-08-25 20:21:07 +0900

    연산자 1 - 산술연산자

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-// 명시적 형 변환
+// 산술 연산자
 
-let num1 = "15";
+let num1 = 10;
 let num2 = 5;
 
-console.log(num1 + num2); // 155
-// JS는 +연산은 숫자형을 문자형으로 자동 변환한다.
-// 나머지는 문자형을 숫자형으로 자동 변환한다.
-
-console.log(parseInt(num1) + num2); // 20 -> 명시적 형 변환
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % 2);
+console.log(num2 % 2);
