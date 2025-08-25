371495b2ad42723e8eb159f9fae3f887b77ba47e
Hany-Kim <hanykim1210@gmail.com>
2025-08-25 20:42:08 +0900

    연산자 4 - 비교 연산자

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-// 대입 연산자
-
-let num = 10;
-num = num + 5;
-
-console.log(num); // 15
-
-// 복합 대입 연산자 : 산술 연산자 + 대입 연산자
-// +=, -=, *=, /=, %=
+// 비교 연산자
+// 일치 : ===, ==
 
 let num1 = 10;
-num1 += 5;
+let num2 = "10";
 
-console.log(num1); // 15
+console.log(num1 === num2); // false, 자료형까지 비교
+console.log(num1 == num2); // true, 형 비교 x 값만 비교 o
+
+// 불일치 : !==, !=
+console.log(num1 !== num2); // true, 자료형까지 비교
+console.log(num1 != num2); // false, 형 비교 x 값만 비교 o
+
+// 대소 비교 : <, >, <=, >=
+let a = 10;
+let b = 20;
+let c = 10;
+
+console.log(a < b); // true
+console.log(a > b); // false
+console.log(a <= b); // true
+console.log(a >= b); // false
