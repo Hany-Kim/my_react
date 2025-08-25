47b9bb38f44d5274a48f1a69b6c2170aab51b4f6
Hany-Kim <hanykim1210@gmail.com>
2025-08-25 09:14:11 +0900

    자료형과 형변환 8 - Undefined

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-// 원시 타입 자료형 - Null
-// 존재 X, 알 수 없는 값
+// 원시 타입 자료형 - Undefined
+// 변수에 값이 할당되지 않은 상태
 
-// 다른 언어에서 Null은 하나의 값으로 활용
-// JS는 오직 Null값만을 포함하는 '자료형'
-
-let num = null;
-console.log(typeof num); // JS의 오래된 오류 | 출력 : object
-console.log(num === null); // === : 비교 연산자 | 출력: true
+let num;
+console.log(typeof num); // undefiend
