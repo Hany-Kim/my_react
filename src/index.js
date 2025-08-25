29a6fb51b19fd452e218011e3d7f986060268cb9
Hany-Kim <hanykim1210@gmail.com>
2025-08-25 20:23:16 +0900

    연산자 2 - 증감연산자

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-// 산술 연산자
+// 증감 연산자
 
 let num1 = 10;
 let num2 = 5;
 
-console.log(num1 + num2);
-console.log(num1 - num2);
-console.log(num1 * num2);
-console.log(num1 / num2);
-console.log(num1 % 2);
-console.log(num2 % 2);
+console.log(num1++); // 10
+console.log(num1); // 11
+console.log(++num1); // 12
+
+console.log(num2--); // 5
+console.log(num2); // 4
+console.log(--num2); // 3
