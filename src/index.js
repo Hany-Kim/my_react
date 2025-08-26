48f19eacce38a0e4ea4ffbf4592d8028b4cd69df
Hany-Kim <hanykim1210@gmail.com>
2025-08-26 09:17:03 +0900

    연산자 6 - 논리 연산자

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
-// 연결 연산자
+// 논리 연산자
+// true false
+// NOT OR AND
 
-let price = 10000;
-console.log("가격: " + price + "원");
+// NOT
+// false -> true, true -> false
+let isClicked = true;
+let isOpened = false;
+
+console.log(!isClicked); // false
+console.log(!isOpened); // true
+
+// OR
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false
+
+// AND
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
