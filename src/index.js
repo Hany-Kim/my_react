ee7643a24c74deeb31c93d91678142ad87bcb8c7
Hany-Kim <hanykim1210@gmail.com>
2025-08-26 13:39:49 +0900

    조건문 2 - switch문

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-// if문
+// switch case
 
-let num = 50;
+let fruit = "apple";
 
-if (num < 10) {
-  console.log("num은 10보다 작습니다.");
-} else if (num > 10) {
-  console.log("num은 10보다 큽니다.");
-} else {
-  console.log("num은 10입니다.");
+switch (fruit) {
+  case "banana":
+    console.log("바나나");
+    break;
+  case "orange":
+    console.log("오렌지");
+    break;
+  case "apple":
+    console.log("사과");
+    break;
+  case "grape":
+    console.log("포도");
+    break;
+  default:
+    console.log("다른 과일");
 }
