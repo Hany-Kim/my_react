d67fb5f5a97bff5ada1190268f62e3c4ce61db9b
Hany-Kim <hanykim1210@gmail.com>
2025-08-26 09:13:55 +0900

    연산자 5 - 연결연산자

--- src/index.js
+++ src/index.js
@@ -1,22 +1,4 @@
-// 비교 연산자
-// 일치 : ===, ==
+// 연결 연산자
 
-let num1 = 10;
-let num2 = "10";
-
-console.log(num1 === num2); // false, 자료형까지 비교
-console.log(num1 == num2); // true, 형 비교 x 값만 비교 o
-
-// 불일치 : !==, !=
-console.log(num1 !== num2); // true, 자료형까지 비교
-console.log(num1 != num2); // false, 형 비교 x 값만 비교 o
-
-// 대소 비교 : <, >, <=, >=
-let a = 10;
-let b = 20;
-let c = 10;
-
-console.log(a < b); // true
-console.log(a > b); // false
-console.log(a <= b); // true
-console.log(a >= b); // false
+let price = 10000;
+console.log("가격: " + price + "원");
