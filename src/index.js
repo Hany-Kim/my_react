bf65e8b576da271230b20076be1c6baa7f39b0dc
Hany-Kim <hanykim1210@gmail.com>
2025-09-08 20:58:53 +0900

    스코프 5 - var키워드

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 // 함수 스코프
 
-function print() {
-  // var는 let과 다르게 함수스코프를 가짐
-  for (var i = 0; i < 10; i++) {
-    console.log(i);
-  }
-  console.log(i); // 10 출력 (오류발생x)
-}
+// let : 블록스코프
+// var : 함수스코프
 
-print();
+/*
+let num = 10;
+let num = 100; // 오류
+
+console.log(num);
+*/
+
+var num = 10;
+var num = 100;
+
+console.log(num); // 100 출력
+/*
+var 키워드
+- 같은 이름의 변수를 여러번 사용 가능, 기존에 선언된 동일한 변수는 무시
+- 오래된 변수 선언 방식 (JS초기 버전에 도입됨)
+- 함수 스코프를 가짐
+
+let / const
+- JS es6버전에 도입된 키워드
+- 블록 스코프를 가짐
+*/
