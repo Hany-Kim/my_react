709badc3ca6fda72764e041a8511af066b994b44
Hany-Kim <hanykim1210@gmail.com>
2025-09-08 20:51:36 +0900

    스코프 2 - 전역, 지역 스코프

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 // 스코프 - 전역, 지역
 
-let globalNum = 100; // 전역 스코프
+let global = "나는 전역 변수입니다"; // 전역 스코프
 
-function testFunc() {
-  let innerNum = 50; // 지역 스코프
-  console.log(globalNum);
-  console.log(innerNum);
+function outerFunction() {
+  let outer = "나는 외부 함수의 변수입니다."; // 지역 스코프
+
+  function innerFunction() {
+    let inner = "나는 내부 함수의 변수입니다."; // 지역 스코프
+    console.log(global);
+    console.log(outer);
+    console.log(inner);
+  }
+
+  innerFunction();
+  console.log(global);
+  console.log(outer);
+  console.log(inner); // Uncaught ReferenceError: inner is not defined at outerFunction (index.js:18:15) at index.js:21:1
 }
 
-testFunc();
-console.log(globalNum);
-console.log(innerNum); // Uncaught ReferenceError: innerNum is not defined at index.js:13:13
+outerFunction();
