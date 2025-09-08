350ac87f4237a775f97a564b498745f92e241ade
Hany-Kim <hanykim1210@gmail.com>
2025-09-08 13:42:34 +0900

    함수 2 - Early return pattern

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-function add(num1, num2) {
-  return num1 + num2;
-  console.log("함수 호출"); // 출력되지 않음.
+// Early return pattern
+
+function compare(num) {
+  if (num === 0) {
+    return "num의 값이 0입니다.";
+  }
+  if (num < 0) {
+    return "num의 값이 0보다 작습니다.";
+  }
+  if (num >= 10) {
+    return "num의 값이 10보다 크거나 같습니다.";
+  }
+  return "num의 값이 0보다 크고 10보다 작습니다.";
 }
 
-let result = add(10, 15);
-console.log(`두 숫자를 더한 결과는 ${result} 입니다.`);
+console.log(compare(15));
