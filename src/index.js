6ae81866e71ecab08abcd8814f4a18c9de5fb9d7
Hany-Kim <hanykim1210@gmail.com>
2025-09-08 13:44:01 +0900

    함수 3 - 함수 실행 시점

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-// Early return pattern
-
-function compare(num) {
-  if (num === 0) {
-    return "num의 값이 0입니다.";
-  }
-  if (num < 0) {
-    return "num의 값이 0보다 작습니다.";
-  }
-  if (num >= 10) {
-    return "num의 값이 10보다 크거나 같습니다.";
-  }
-  return "num의 값이 0보다 크고 10보다 작습니다.";
+function print() {
+  console.log(1);
+  console.log(2);
 }
 
-console.log(compare(15));
+console.log(3);
+print();
+console.log(4);
+
+// 3
+// 1
+// 2
+// 4
