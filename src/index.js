a2522e941a51a7e88cf4eab55919c77f789cc30d
Hany-Kim <hanykim1210@gmail.com>
2025-09-15 05:25:54 +0900

    함수 표현식과 화살표 함수 4 - 차이점

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-// 함수 선언식 : function키워드 옆에 함수의 이름을 작성
+// 함수 선언식 vs 함수 표현식 - 호이스팅
 
-/*
-JS에서 함수를 선언하는 방법
-1. 함수 선언식
-2. 함수 표현식
-*/
+// 함수 선언식 : 호이스팅으로 정상 동작
+hoisted1();
 
-function func() {
-  console.log("hello javascript");
-}
+function hoisted1() {
+  console.log("hoisting");
+} // hoisting 출력
+
+// 함수 표현식 : hoisted2라는 변수에 함수를 할당하는 형태, const 변수의 경우 선언시점에 TDZ에 올라가 있기때문에, 호이스팅되지 않는다.
+hoisted2(); // index.js:11 Uncaught ReferenceError: Cannot access 'hoisted2' before initialization at index.js:11:1
+
+const hoisted2 = function () {
+  console.log("hoisting");
+};
