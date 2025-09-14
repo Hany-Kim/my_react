278abe7759fba59206efd072c79faabde01acca7
Hany-Kim <hanykim1210@gmail.com>
2025-09-15 05:47:04 +0900

    객체 2 - 객체의 속성

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-// 객체 생성 방법 : 생성자 함수, 리터럴
+// 객체의 속성
+// 객체의 프로퍼티는 객체의 중괄호 내부에 들어갈 데이터
+// key : value 쌍 구조
 
-// 생성자 함수
-let obj1 = new Object();
-console.log(obj1); // {} 출력
-
-// 리터럴
-let obj2 = {};
-console.log(obj2); // {} 출력
-
-// JS에서 객체는 {}로 표현할 수 있다.
+let book = {
+  title: "자바스크립트 첫걸음",
+  author: "김주한",
+  category: "자바스크립트",
+  year: undefined,
+  color: function () {
+    console.log("orange");
+  },
+};
