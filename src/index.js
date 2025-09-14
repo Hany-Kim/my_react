a5322160c897367dfc98b266537d105079767675
Hany-Kim <hanykim1210@gmail.com>
2025-09-15 06:04:07 +0900

    객체 6 - 객체의 속성 삭제방법

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
-// 객체의 속성 추가방법 : 점 표기법, 괄호 표기법
+// 객체의 속성 삭제방법 : 점 표기법, 괄호 표기법
 
-let cat = {
-  age: 2,
+const cat = { age: 2, name: "야옹이", color: "white" };
+delete cat.color;
+delete cat["age"];
+
+console.log(cat); // {name: '야옹이'}
+
+const person = {
+  name: "홍길동",
+  age: 23,
+  print: function () {
+    console.log("제 이름은 ${this.name}입니다. ");
+  },
 };
 
-cat.name = "야옹이";
-cat["color"] = "white";
-
-console.log(cat); // {age: 2, name: '야옹이', color: 'white'}
-
-cat.age = 3; // -> 값 업데이트
-cat["color"] = "black";
-console.log(cat); // {age: 3, name: '야옹이', color: 'black'}
-
-cat = {
-  age: 2,
-};
-console.log(cat);
-
-const cat1 = { age: 2, name: "야옹이", color: "white" };
-cat1 = { age: 3, name: "야옹이", color: "black" }; // index.js:22 Uncaught TypeError: Assignment to constant variable. at index.js:22:6
-// 객체는 let 또는 const로 초기화해도 프로퍼티 업데이트는 가능
+person.print();
+person["print"]();
