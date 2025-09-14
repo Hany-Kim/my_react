ca1936c0ad3d852da158fafee270a17af1353533
Hany-Kim <hanykim1210@gmail.com>
2025-09-15 05:50:26 +0900

    객체 2 - 객체의 속성 사용방법

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-// 객체의 속성
-// 객체의 프로퍼티는 객체의 중괄호 내부에 들어갈 데이터
-// key : value 쌍 구조
-
-let book = {
-  title: "자바스크립트 첫걸음",
-  author: "김주한",
-  category: "자바스크립트",
-  year: undefined,
-  color: function () {
-    console.log("orange");
-  },
+// 객체의 속성 사용방법 : 점 표기법, 괄호 표기법
+let car = {
+  name: "붕붕",
+  model: "morning",
+  color: "black",
 };
+
+// 점 표기법
+console.log(car.name);
+console.log(car.color);
+
+// 괄호 표기법
+console.log(car["name"]);
+console.log(car["color"]);
