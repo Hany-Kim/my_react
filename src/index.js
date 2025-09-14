27bdf7169744d02b6e5fe6f7e90be4c2d828f854
Hany-Kim <hanykim1210@gmail.com>
2025-09-15 05:59:35 +0900

    객체 5 - 객체의 속성 수정방법

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-// 객체의 속성 사용방법 : 점 표기법, 괄호 표기법
-let car = {
-  name: "붕붕",
-  model: "morning",
-  color: "black",
+// 객체의 속성 추가방법 : 점 표기법, 괄호 표기법
+
+let cat = {
+  age: 2,
 };
 
-// 점 표기법
-console.log(car.name);
-console.log(car.color);
+cat.name = "야옹이";
+cat["color"] = "white";
 
-// 괄호 표기법
-console.log(car["name"]);
-console.log(car["color"]);
+console.log(cat); // {age: 2, name: '야옹이', color: 'white'}
 
-const getValue = (key) => {
-  console.log(car[key]);
+cat.age = 3; // -> 값 업데이트
+cat["color"] = "black";
+console.log(cat); // {age: 3, name: '야옹이', color: 'black'}
+
+cat = {
+  age: 2,
 };
-getValue("color");
+console.log(cat);
+
+const cat1 = { age: 2, name: "야옹이", color: "white" };
+cat1 = { age: 3, name: "야옹이", color: "black" }; // index.js:22 Uncaught TypeError: Assignment to constant variable. at index.js:22:6
+// 객체는 let 또는 const로 초기화해도 프로퍼티 업데이트는 가능
