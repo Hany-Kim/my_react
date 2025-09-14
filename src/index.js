c5f46e1da00e3659bfe59ad19a2a9dda2e16100f
Hany-Kim <hanykim1210@gmail.com>
2025-09-15 05:43:39 +0900

    객체 1 - 생성 방법

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-// 콜백 함수 : 다른 함수의 매개변수로 넘겨준 함수
+// 객체 생성 방법 : 생성자 함수, 리터럴
 
-const testFunc = (callback) => {
-  callback();
-};
+// 생성자 함수
+let obj1 = new Object();
+console.log(obj1); // {} 출력
 
-testFunc(() => {
-  console.log("콜백 함수 테스트");
-});
+// 리터럴
+let obj2 = {};
+console.log(obj2); // {} 출력
 
-testFunc(function () {
-  console.log("콜백 함수 테스트");
-});
+// JS에서 객체는 {}로 표현할 수 있다.
