7a37a72bde6fd518b82dd237530abd59d9a1ef5a
Hany-Kim <hanykim1210@gmail.com>
2025-09-15 05:33:02 +0900

    함수 표현식과 화살표 함수 6 - 콜백함수 (중복 코드 제거 후)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 // 콜백 함수 : 다른 함수의 매개변수로 넘겨준 함수
 
-const printResult = (a, b) => {
+const calculate = (a, b, callback) => {
   let result = a + b;
+  callback(result);
+};
+
+const printResult = (result) => {
   console.log("결과: " + result);
 };
-const doubleResult = (a, b) => {
-  let result = a + b;
+const doubleResult = (result) => {
   console.log("결과에 2를 곱한 값: " + result * 2);
 };
 
-printResult(5, 3);
-doubleResult(5, 3);
+calculate(5, 3, printResult);
+calculate(5, 3, doubleResult);
