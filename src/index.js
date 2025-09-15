646bdfb06ccf1bbadf350afa2a3a42d95b67c35f
Hany-Kim <hanykim1210@gmail.com>
2025-09-15 21:00:27 +0900

    반복문 4 - 객체 탐색 (Object.keys())

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-// 반복문 - 배열 탐색
+// 반복문 - 객체 탐색
 
-let arr = [1, 2, 3, 4, 5];
+let person = {
+  name: "홍길동",
+  age: 25,
+  height: 180,
+};
 
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
+// Object.keys(person) : 객체의 key값을 배열형태로 반환
+console.log(Object.keys(person));
+
+let newArray = Object.keys(person);
+for (let i = 0; i < newArray.length; i++) {
+  let nowKey = newArray[i];
+  console.log(`key: ${nowKey}, value: ${person[nowKey]}`);
 }
