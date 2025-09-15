33ccc0a919bbcff2cad4259747d030b64304fd7a
Hany-Kim <hanykim1210@gmail.com>
2025-09-15 20:03:24 +0900

    배열 2 - 사용 방법

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 // 배열
 // : 순서가 있는 요소들의 집합, 여러개의 데이터가 모인 리스트
+// key값 대신 index로 접근
 
-// 배열 생성 방법
-// 1. 생성자 함수
-// 2. 리터럴
-
-// 생성자 함수
-let arr1 = new Array(1, 2, 3);
-let arr2 = new Array(3); // 길이가 3인 비어있는 배열
-console.log(arr1); // [1, 2, 3]
-console.log(arr2); // [비어 있음 × 3]
-
-// 리터럴
-let arr3 = [1, 2, 3];
-let arr4 = [3];
-console.log(arr3); // [1, 2, 3]
-console.log(arr4); // [3]
+let array = [1, "hello", null];
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
