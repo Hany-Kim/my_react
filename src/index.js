7faaeba256ad7aac2120841e640cc0c4460ddb87
Hany-Kim <hanykim1210@gmail.com>
2025-09-15 21:05:38 +0900

    반복문 7 - for-of문

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-// 반복문 - 객체 탐색
+// 반복문 - for-of문
+// 모든 요소에 접근할 때 사용
 
-let person = {
-  name: "홍길동",
-  age: 25,
-  height: 180,
-};
-
-// Object.entries(person) : 객체의 key와 value 쌍을 배열형태로 반환
-console.log(Object.entries(person));
-
-let newArray = Object.entries(person);
-for (let i = 0; i < newArray.length; i++) {
-  console.log(`key: ${newArray[i][0]}, value: ${newArray[i][1]}`);
+let arr = [1, 2, 3, 4, 5];
+for (let i of arr) {
+  console.log(i);
 }
