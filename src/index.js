331e443a1521f7fff9dae46b2e0f6629ba95bf82
Hany-Kim <hanykim1210@gmail.com>
2025-09-15 20:08:52 +0900

    배열 4 - 수정 방법

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-// 배열 추가
-let fruits = ["apple", "orange", "peach"];
+// 배열 수정
+let animal = ["cat", "dog", "hamster"];
 
-fruits.push("grape"); // 배열 뒤에 추가
+animal = ["cat", "rabbit", "hamster"];
+animal[2] = "parrot";
 
-console.log(fruits); // (4) ['apple', 'orange', 'peach', 'grape']
-
-fruits.unshift("grape1"); // 배열 앞에 추가
-
-console.log(fruits); // (5) ['grape1', 'apple', 'orange', 'peach', 'grape']
+console.log(animal); // (3) ['cat', 'rabbit', 'parrot']
