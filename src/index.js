dda2ab913ec76cf1949f88651c77cb5138c7094d
Hany-Kim <hanykim1210@gmail.com>
2025-09-15 20:55:01 +0900

    반복문 1 - for문

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
-// 생성자 함수
-// 특징 : new 키워드를 사용항
-
-let obj = new Object();
-let arr = new Array();
-
-// 생성자함수는 일반적으로 동일한 구조의 여러 객체를 쉽게 생성하기 위해 사용
-function Person(name, age, job) {
-  this.name = name;
-  this.age = age;
-  this.job = job;
-  this.sayHi = function () {
-    console.log("안녕하세요! " + this.name + "입니다.");
-  };
+// 반복문 - for문
+for (let i = 1; i < 6; i++) {
+  console.log(i);
 }
 
-const person1 = new Person("홍길동", 30, "Manager");
-const person2 = new Person("김철수", 25, "Designer");
-
-console.log(person1.name);
-console.log(person2.age);
-person1.sayHi();
-person2.sayHi();
+for (let i = 5; i >= 0; i--) {
+  console.log(i);
+}
