be6190527947cbacbe162104727dbbd7161d0392
Hany-Kim <hanykim1210@gmail.com>
2025-09-15 20:30:40 +0900

    생성자 함수 2 - 장점

--- src/index.js
+++ src/index.js
@@ -4,22 +4,19 @@
 let obj = new Object();
 let arr = new Array();
 
-const person1 = {
-  name: "홍길동",
-  age: 30,
-  job: "Manager",
-  sayHi: function () {
+// 생성자함수는 일반적으로 동일한 구조의 여러 객체를 쉽게 생성하기 위해 사용
+function Person(name, age, job) {
+  this.name = name;
+  this.age = age;
+  this.job = job;
+  this.sayHi = function () {
     console.log("안녕하세요! " + this.name + "입니다.");
-  },
-};
-const person2 = {
-  name: "김철수",
-  age: 25,
-  job: "Designer",
-  sayHi: function () {
-    console.log("안녕하세요! " + this.name + "입니다.");
-  },
-};
+  };
+}
+
+const person1 = new Person("홍길동", 30, "Manager");
+const person2 = new Person("김철수", 25, "Designer");
+
 console.log(person1.name);
 console.log(person2.age);
 person1.sayHi();
