c2c86f376116736c261e4fe10a3ca136595327f4
Hany-Kim <hanykim1210@gmail.com>
2025-10-13 20:17:21 +0900

    배열내장함수 10 - concat

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 // 배열 내장 함수
-// 배열에서 특정 조건을 만족하는 값들만 추철 (필터링)
 
-let colors = [
-  { id: 1, color: "green" },
-  { id: 2, color: "blue" },
-  { id: 3, color: "purple" },
-  { id: 4, color: "yellow" },
-];
+let array1 = ["green", "blue"];
+let array2 = ["purple", "yellow"];
 
-let sliceArray = colors.slice(1, 3);
-console.log(sliceArray); // { id: 2, color: "blue" }, { id: 3, color: "purple" }
+console.log(array1.concat(array2)); // ['green', 'blue', 'purple', 'yellow']
