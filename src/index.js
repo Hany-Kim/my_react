b1c6815a1b69a4ef17db91b5deae9d4a699f2a35
Hany-Kim <hanykim1210@gmail.com>
2025-10-13 20:01:30 +0900

    배열 7 - find 내장함수

--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
 // 배열 내장 함수
 
-// indexOf메서드 : 매개변수로 받은 요소가 배열의 몇번째에 있는지
-// 배열의 요소값이 객체나 배열이면 못 찾음.
+// find 메서드 : 찾아낸 값의 인덱스가 아닌, 값 그자체를 리턴
 
-// findIndex : 배열의 요소 값이 객체라면, 어떤 요소가 배열의 몇번째인지 찾기 위해서
 let colors = [
   { id: 1, color: "green" },
   { id: 2, color: "blue" },
   { id: 3, color: "purple" },
 ];
-console.log(colors.indexOf("blue", 1)); // -1
 
-let idx = colors.findIndex((elm) => elm.color === "purple");
-console.log(idx); // 2
+let idx = colors.find((elm) => elm.color === "purple");
 
-colors.findIndex((elm, idx, array) =>
-  console.log(`${idx} 번째 값은 id: ${elm.id}, color: ${elm.color}`)
-);
-// 출력
-// 0 번째 값은 id: 1, color: green
-// 1 번째 값은 id: 2, color: blue
-// 2 번째 값은 id: 3, color: purple
-
-colors.findIndex((elm, idx, array) => console.log(console.log(array)));
-// 객체 출력
+console.log(idx); // {id: 3, color: 'purple'}
