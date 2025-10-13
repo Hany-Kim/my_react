c66755a15b9691b6db3b394e1468f16f7bd86c65
Hany-Kim <hanykim1210@gmail.com>
2025-10-13 19:29:49 +0900

    배열 4 - includes 내장함수

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 // 배열 내장 함수
 
-// at메서드 : 인덱스 참조
+// includes메서드 : 매개변수로 받은 요소를 배열이 포함하고 있는지 판별 후 boolean으로 반환
 let colors = ["green", "blue", "purple"];
-console.log(colors[2]); // purple
-console.log(colors[colors.length - 1]); // purple
-console.log(colors.at(1)); // blue
-console.log(colors.at(-1)); // purple
+console.log(colors.includes("blue")); // true
+console.log(colors.includes("red")); // false
