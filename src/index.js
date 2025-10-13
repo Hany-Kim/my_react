69841b97aeff4a4939417ce129d2d31cb3b26cd7
Hany-Kim <hanykim1210@gmail.com>
2025-10-13 19:59:22 +0900

    배열 6 - findIndex 내장함수

--- src/index.js
+++ src/index.js
@@ -3,16 +3,24 @@
 // indexOf메서드 : 매개변수로 받은 요소가 배열의 몇번째에 있는지
 // 배열의 요소값이 객체나 배열이면 못 찾음.
 
-let colors = ["green", "blue", "purple"];
-console.log(colors.indexOf("purple")); // 2
-console.log(colors.indexOf("yellow")); // -1
-
-// (찾을 원소, 찾기 시작할 위치)
-console.log(colors.indexOf("blue", 1)); // 1
-console.log(colors.indexOf("blue", 2)); // -1
-
-let colorsObj = [
-  { id: 1, colors: "green" },
-  { id: 2, colors: "blue" },
-  { id: 3, colors: "purple" },
+// findIndex : 배열의 요소 값이 객체라면, 어떤 요소가 배열의 몇번째인지 찾기 위해서
+let colors = [
+  { id: 1, color: "green" },
+  { id: 2, color: "blue" },
+  { id: 3, color: "purple" },
 ];
+console.log(colors.indexOf("blue", 1)); // -1
+
+let idx = colors.findIndex((elm) => elm.color === "purple");
+console.log(idx); // 2
+
+colors.findIndex((elm, idx, array) =>
+  console.log(`${idx} 번째 값은 id: ${elm.id}, color: ${elm.color}`)
+);
+// 출력
+// 0 번째 값은 id: 1, color: green
+// 1 번째 값은 id: 2, color: blue
+// 2 번째 값은 id: 3, color: purple
+
+colors.findIndex((elm, idx, array) => console.log(console.log(array)));
+// 객체 출력
