003b0226641942a5b9cf41404c5ecb57b044d51f
Hany-Kim <hanykim1210@gmail.com>
2025-10-13 19:27:58 +0900

    배열 3 - at 내장함수

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
 // 배열 내장 함수
 
-let arr = [1, 2, 3, 4, 5];
-
-// 배열 원소내의 값을 일괄적으로 바꿈
-let newArray = [];
-
-for (let i = 0; i < arr.length; i++) {
-  newArray.push(arr[i] * 10);
-}
-
-console.log(newArray);
-//[10, 20, 30, 40, 50]
-
-// Map내장함수를 활용하면 위 과정이 간소화 된다.
-let newArrayMap = arr.map((elm) => {
-  return elm * 10;
-});
-
-console.log(newArrayMap);
-// [10, 20, 30, 40, 50]
+// at메서드 : 인덱스 참조
+let colors = ["green", "blue", "purple"];
+console.log(colors[2]); // purple
+console.log(colors[colors.length - 1]); // purple
+console.log(colors.at(1)); // blue
+console.log(colors.at(-1)); // purple
