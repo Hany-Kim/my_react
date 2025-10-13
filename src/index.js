be1127dcfeb99b42399b863c5590ac2d5812343f
Hany-Kim <hanykim1210@gmail.com>
2025-10-13 19:13:31 +0900

    배열 2 - Map 내장함수

--- src/index.js
+++ src/index.js
@@ -2,51 +2,20 @@
 
 let arr = [1, 2, 3, 4, 5];
 
-// 기본 반복문
+// 배열 원소내의 값을 일괄적으로 바꿈
+let newArray = [];
+
 for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
+  newArray.push(arr[i] * 10);
 }
 
-// for-each
-// for-each 내장함수는 매개변수로 함수를 전달할 수 있다.
-// 콜백 : 함수를 매개변수로 전달
-// for-each 콜백함수에는 3가지 파라미터를 전달할 수 있다. => (CurrentValue, index, array)
-// CurrentValue : 처리할 현재 요소를 매개변수로 전달.
-// index : 실제 배열 요소의 순서
-// array : 실제 배열 자체를 출력
+console.log(newArray);
+//[10, 20, 30, 40, 50]
 
-arr.forEach((elm) => {
-  console.log(elm);
+// Map내장함수를 활용하면 위 과정이 간소화 된다.
+let newArrayMap = arr.map((elm) => {
+  return elm * 10;
 });
-// 출력
-// 1
-// 2
-// 3
-// 4
-// 5
 
-arr.forEach((elm, idx) => {
-  console.log(`${idx}번째 요소는 ${elm}입니다.`);
-});
-// 출력
-// 0번째 요소는 1입니다.
-// 1번째 요소는 2입니다.
-// 2번째 요소는 3입니다.
-// 3번째 요소는 4입니다.
-// 4번째 요소는 5입니다.
-
-arr.forEach((elm, idx, array) => {
-  console.log(`${idx}번째 요소는 ${elm}입니다.`);
-  console.log(array);
-});
-// 출력
-// 0번째 요소는 1입니다.
-// [1, 2, 3, 4, 5]
-// 1번째 요소는 2입니다.
-// [1, 2, 3, 4, 5]
-// 2번째 요소는 3입니다.
-// [1, 2, 3, 4, 5]
-// 3번째 요소는 4입니다.
-// [1, 2, 3, 4, 5]
-// 4번째 요소는 5입니다.
-// [1, 2, 3, 4, 5]
+console.log(newArrayMap);
+// [10, 20, 30, 40, 50]
