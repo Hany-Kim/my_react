1a7fd289570eb36a4a9e24e9b7e14fab0144c292
Hany-Kim <hanykim1210@gmail.com>
2025-10-20 03:12:14 +0900

    배열내장함수 14 - isArray

--- src/index.js
+++ src/index.js
@@ -1,28 +1,8 @@
 // 배열 내장 함수
 
-let numbers = [1, 100, 25, 50];
-let sum = 0;
+let a = Array.isArray([1, 100, 50]); // true
+let b = Array.isArray([{ id: 1, color: "green" }]); // true
+let c = Array.isArray("string"); // false
+let d = Array.isArray(undefined); // false
 
-// 누적합
-numbers.forEach((elm) => {
-  sum += elm;
-});
-
-console.log(sum); // 176
-
-// acc : 콜백함수 반환값 누적
-// cur : 현재 처리할 요소 값
-// idx : 처리한 현재 요소의 인덱스
-let reduceSum = numbers.reduce((acc, cur, idx) => {
-  console.log(acc, cur, idx);
-  return acc + cur;
-}, 0);
-
-console.log(reduceSum); // 176
-
-let reduceSum1 = numbers.reduce((acc, cur, idx) => {
-  console.log(acc, cur, idx);
-  return acc + cur;
-}, 10);
-
-console.log(reduceSum1); // 186
+console.log(a, b, c, d);
