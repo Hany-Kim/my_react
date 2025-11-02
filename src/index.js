4207ab8857a358656610bb2fbe80119ba96e82be
Hany-Kim <hanykim1210@gmail.com>
2025-11-03 00:54:43 +0900

    spread 1 - 객체

--- src/index.js
+++ src/index.js
@@ -1,50 +1,52 @@
-// 구조 분해 할당
-
-let colors = {
-  c1: "green",
-  c2: "blue",
-  c3: "purple",
+// spread (확산, 퍼짐, 전파)
+// : 특정 배열의 원소나 객체의 프로퍼티 값을 펼침
+const toy = {
+  type: "bear",
+  price: 15000,
 };
 
-// 일반 적인 객체 분해 할당
-let c1 = colors.c1;
-let c2 = colors.c2;
-let c3 = colors.c3;
+// 반복되는 프로퍼티 값이 많으면 번거로운 작업이 됨.
+// const blueToy = {
+//   type: 'bear',
+//   price: 15000,
+//   color: 'blue'
+// }
 
-console.log(c1);
-console.log(c2);
-console.log(c3);
+// const yellowToy = {
+//   type: 'bear',
+//   price: 15000,
+//   color: 'yellow'
+// }
 
-// 객체 구조 분해 할당 방법 1
-let ncolors = {
-  nc1: "green",
-  nc2: "blue",
-  nc3: "purple",
+const blueToy = {
+  ...toy,
+  color: "blue",
 };
 
-let { nc1, nc2, nc3 } = ncolors;
-
-console.log(nc1);
-console.log(nc2);
-console.log(nc3);
-
-// 객체 구조 분해 할당 2
-// 객체 프로퍼티의 값을 할당하는 변수가 객체 프로퍼티의 키 값과 다를 때, 비구조 할당 사용
-let { c1: color1, c2: color2, c3: color3 } = colors;
-
-console.log(color1);
-console.log(color2);
-console.log(color3);
-
-// 변수의 수가 객체 프로퍼티 수보다 많을 때, 기본값 설정
-let nncolors = {
-  nnc1: "green",
-  nnc2: "blue",
-  nnc3: "purple",
+const yellowToy = {
+  ...toy,
+  color: "yellow",
 };
-let { nnc1, nnc2, nnc3, nnc4 = "yellow" } = nncolors;
 
-console.log(nnc1);
-console.log(nnc2);
-console.log(nnc3);
-console.log(nnc4);
+console.log(blueToy);
+// 출력
+// color
+// :
+// "blue"
+// price
+// :
+// 15000
+// type
+// :
+// "bear"
+console.log(yellowToy);
+// 출력
+// color
+// :
+// "yellow"
+// price
+// :
+// 15000
+// type
+// :
+// "bear"
