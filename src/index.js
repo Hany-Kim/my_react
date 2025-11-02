75835b641b244a381e6a9b74fd213712b70286ca
Hany-Kim <hanykim1210@gmail.com>
2025-11-03 02:14:50 +0900

    동기와 비동기 2 - 비동기 setTimeout

--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
-// 동기
-// 함수가 순차적으로 실행됨
+// 비동기 - setTimeout() 내장 함수
 
-const workA = () => {
-  console.log("workA");
-};
-const workB = () => {
-  console.log("workB");
-};
-const workC = () => {
-  console.log("workC");
+// setTimeout(() => {}, delayTime) -> (콜백함수, 지연시간)
+setTimeout(() => {
+  console.log("비동기");
+}, 3000);
+
+console.log("종료");
+
+// 출력 결과
+// 종료
+// 비동기
+
+setTimeout(() => {
+  console.log("비동기");
+  console.log("종료");
+}, 3000);
+
+// 출력 결과
+// 비동기
+// 종료
+
+// ---------------
+// '비동기'를 출력하는 함수(setTimeout)가 하나의 함수(work)로 이루어져 있다면
+// '종료'를 출력하는 함수는 work함수에 콜백함수로 전달해 비동기롤 작업을 수행할 수 있다.
+const work = (callback) => {
+  setTimeout(() => {
+    console.log("비동기");
+    callback();
+  }, 3000);
 };
 
-workA();
-workB();
-workC();
+work(() => {
+  console.log("종료");
+});
+
+// 출력 결과
+// 비동기
+// 종료
