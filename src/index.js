9da03f7a8acb974263e80152b269af756a8c5c0e
Hany-Kim <hanykim1210@gmail.com>
2025-11-03 02:24:31 +0900

    동기와 비동기 3 - 동기와 비동기 비교 setTimeout()

--- src/index.js
+++ src/index.js
@@ -1,39 +1,38 @@
-// 비동기 - setTimeout() 내장 함수
+// 동기와 비동기 - setTimeout() 내장 함수
 
-// setTimeout(() => {}, delayTime) -> (콜백함수, 지연시간)
-setTimeout(() => {
-  console.log("비동기");
-}, 3000);
-
-console.log("종료");
-
-// 출력 결과
-// 종료
-// 비동기
-
-setTimeout(() => {
-  console.log("비동기");
-  console.log("종료");
-}, 3000);
-
-// 출력 결과
-// 비동기
-// 종료
-
-// ---------------
-// '비동기'를 출력하는 함수(setTimeout)가 하나의 함수(work)로 이루어져 있다면
-// '종료'를 출력하는 함수는 work함수에 콜백함수로 전달해 비동기롤 작업을 수행할 수 있다.
-const work = (callback) => {
+const workA = () => {
+  // 비동기
   setTimeout(() => {
-    console.log("비동기");
-    callback();
+    console.log("workA");
+  }, 5000);
+};
+
+const workB = () => {
+  // 비동기
+  setTimeout(() => {
+    console.log("workB");
   }, 3000);
 };
 
-work(() => {
-  console.log("종료");
-});
+const workC = () => {
+  // 비동기
+  setTimeout(() => {
+    console.log("workC");
+  }, 10000);
+};
+
+const workD = () => {
+  // 동기
+  console.log("workD");
+};
+
+workA();
+workB();
+workC();
+workD();
 
 // 출력 결과
-// 비동기
-// 종료
+// workD
+// workB
+// workA
+// workC
