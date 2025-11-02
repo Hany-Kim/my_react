3a2b6537c793607a657dffae432082aa497288e1
Hany-Kim <hanykim1210@gmail.com>
2025-11-03 01:57:10 +0900

    rest 3 - 함수

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 // rest (나머지 매개변수)
 // 특정 부분들을 하나의 객체나 배열로 묶어줌.
 
-const color = ["red", "orance", "yellow", "green"];
-const [c1, c2, ...rest] = color;
-console.log(c1, c2);
-console.log(rest); // ['yellow', 'green']
+// 함수에서 사용 법
+const print = (a, b, c, d, e, f) => {
+  console.log([c, d, e, f]); // [3, 4, 5, 6]
+};
+
+print(1, 2, 3, 4, 5, 6);
+
+// 더 많은 매개변수를 받아야 한다면?
+const print1 = (a, b, ...rest) => {
+  console.log([a, b, rest]);
+  // 0: 1
+  // 1: 2
+  // 2: (4) [3, 4, 5, 6]
+};
+print1(1, 2, 3, 4, 5, 6);
