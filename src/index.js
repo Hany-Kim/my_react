ee4971a1ef5430a520f870bd7ea289b5500b49f0
Hany-Kim <hanykim1210@gmail.com>
2025-11-03 02:01:54 +0900

    spread vs rest 비교

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-// rest (나머지 매개변수)
-// 특정 부분들을 하나의 객체나 배열로 묶어줌.
-
-// 함수에서 사용 법
+// 함수에서 spread, rest 모두 사용
 const print = (a, b, c, d, e, f) => {
-  console.log([c, d, e, f]); // [3, 4, 5, 6]
+  console.log(a, b, c, d, e, f);
 };
+const numbers = [1, 2, 3, 4, 5, 6];
+print(numbers[0], numbers[1], numbers[2], numbers[3], numbers[4], numbers[5]); // 1 2 3 4 5 6
 
-print(1, 2, 3, 4, 5, 6);
+// 매개변수가 늘어날수록 번거로워진다.
+// spread는 인수로 많은 값을 넘겨줄 때 흔히 사용
+print(...numbers); // 1 2 3 4 5 6
 
-// 더 많은 매개변수를 받아야 한다면?
-const print1 = (a, b, ...rest) => {
-  console.log([a, b, rest]);
-  // 0: 1
-  // 1: 2
-  // 2: (4) [3, 4, 5, 6]
+// numbers배열의 크기가 6개인것을 알고 있기 때문에, print() 파라미터로 6개 작성
+// numbers배열의 크기가 바뀌면, print()에서 오류가 발생할 가능성이 있음.
+// 매개변수가 많으면 하나하나 작성하기 번거로움
+const print1 = (...rest) => {
+  console.log(...rest);
 };
-print1(1, 2, 3, 4, 5, 6);
+print1(...numbers); // 1 2 3 4 5 6
