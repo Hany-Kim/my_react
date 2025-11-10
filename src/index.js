1e35e51db2343886dab879150eeb0e994244f1f1
Hany-Kim <hanykim1210@gmail.com>
2025-11-10 19:46:37 +0900

    프로미스 객체 2 - 콜백지옥

--- src/index.js
+++ src/index.js
@@ -1,37 +1,32 @@
 // 프로미스 객체
-// : JS에서 비동기 작업을 조금 더 편리하게 처리할 수 있음.
-// : JS의 비동기 처리방식의 문제점인 콜백지옥을 해결하기 위해 프로미스 객체를 이용.
+// 콜백 지옥
 
-const executor = (resolve, reject) => {
-  // executor는 작업처리의 성공여부에 따라
-  // 성공 -> resolve 함수 호출
+const workA = (value, callback) => {
   setTimeout(() => {
-    resolve("성공");
-    reject("실패");
+    callback(value + 5); // 인수로 전달받은 callback() 실행
+  }, 5000);
+};
+const workB = (value, callback) => {
+  setTimeout(() => {
+    callback(value - 3);
   }, 3000);
-  // 실패 -> reject 함수 호출
 };
 
-const promise = new Promise(executor);
+const workC = (value, callback) => {
+  setTimeout(() => {
+    callback(value + 10);
+  }, 10000);
+};
 
-// 방법 1
-// promise.then( 성공일 때, 실패일 때 );
-promise.then(
-  (result) => {
-    console.log(result);
-  },
-  (error) => {
-    console.log(error);
-  }
-); // '성공' 출력
-
-// 방법 2
-promise
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((error) => {
-    console.log(error);
-  }); // '성공' 출력
-
-console.log(promise);
+// 콜백지옥
+workA(10, (resA) => {
+  console.log(`workA : ${resA}`);
+  workB(resA, (resB) => {
+    console.log(`workB : ${resB}`);
+    workC(resB, (resC) => {
+      console.log(`workC : ${resC}`);
+    });
+  });
+});
+// 이렇게 작성하면 여러 작업을 비동기로 처리하더라도 순서대로 처리할 수 있다.
+// 비동기 함수의 특정 결과값을 다른 비동기  함수에서 활용할 수 있다.
