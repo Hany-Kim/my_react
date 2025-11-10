2024d2fec765ea2e6759f28b76ef36c4cda243aa
Hany-Kim <hanykim1210@gmail.com>
2025-11-10 19:12:38 +0900

    프로미스 객체 1

--- src/index.js
+++ src/index.js
@@ -1,38 +1,37 @@
-// 동기와 비동기 - setTimeout() 내장 함수
+// 프로미스 객체
+// : JS에서 비동기 작업을 조금 더 편리하게 처리할 수 있음.
+// : JS의 비동기 처리방식의 문제점인 콜백지옥을 해결하기 위해 프로미스 객체를 이용.
 
-const workA = () => {
-  // 비동기
+const executor = (resolve, reject) => {
+  // executor는 작업처리의 성공여부에 따라
+  // 성공 -> resolve 함수 호출
   setTimeout(() => {
-    console.log("workA");
-  }, 5000);
-};
-
-const workB = () => {
-  // 비동기
-  setTimeout(() => {
-    console.log("workB");
+    resolve("성공");
+    reject("실패");
   }, 3000);
+  // 실패 -> reject 함수 호출
 };
 
-const workC = () => {
-  // 비동기
-  setTimeout(() => {
-    console.log("workC");
-  }, 10000);
-};
+const promise = new Promise(executor);
 
-const workD = () => {
-  // 동기
-  console.log("workD");
-};
+// 방법 1
+// promise.then( 성공일 때, 실패일 때 );
+promise.then(
+  (result) => {
+    console.log(result);
+  },
+  (error) => {
+    console.log(error);
+  }
+); // '성공' 출력
 
-workA();
-workB();
-workC();
-workD();
+// 방법 2
+promise
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  }); // '성공' 출력
 
-// 출력 결과
-// workD
-// workB
-// workA
-// workC
+console.log(promise);
