4dc447d544e31a0d755618a774f10531829dd4f9
Hany-Kim <hanykim1210@gmail.com>
2025-11-10 20:48:08 +0900

    async와 await 2 - async와 await

--- src/index.js
+++ src/index.js
@@ -9,11 +9,13 @@ const delay = (ms) => {
   });
 };
 
-// async와 await 사용 X
-const start = () => {
-  delay(3000).then((result) => {
-    console.log(result);
-  });
+// async와 await 사용
+// async는 비동기를 사용할 함수의 오른쪽에 작성
+// async를 사용하면 해당 함수는 항상 Promise객체를 반환
+// await은 async함수를 작성한 내부에서 사용
+const start = async () => {
+  let result = await delay(3000);
+  console.log(result);
 };
 
 start();
