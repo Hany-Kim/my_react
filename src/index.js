d8884cea4c8d65427471521a2b7501b7f539b68b
Hany-Kim <hanykim1210@gmail.com>
2025-11-10 19:49:45 +0900

    프로미스 객체 3 - 프로미스 객체를 사용했지만 콜백지옥

--- src/index.js
+++ src/index.js
@@ -1,32 +1,39 @@
 // 프로미스 객체
 // 콜백 지옥
 
-const workA = (value, callback) => {
-  setTimeout(() => {
-    callback(value + 5); // 인수로 전달받은 callback() 실행
-  }, 5000);
+const workA = (value) => {
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(value + 5);
+    }, 5000);
+  });
+  return promise;
 };
-const workB = (value, callback) => {
-  setTimeout(() => {
-    callback(value - 3);
-  }, 3000);
+const workB = (value) => {
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(value - 3);
+    }, 3000);
+  });
+  return promise;
 };
 
-const workC = (value, callback) => {
-  setTimeout(() => {
-    callback(value + 10);
-  }, 10000);
+const workC = (value) => {
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(value + 10);
+    }, 10000);
+  });
+  return promise;
 };
 
-// 콜백지옥
-workA(10, (resA) => {
+// 프로미스 객체를 사용했지만 여전히 콜백지옥 형태와 유사
+workA(10).then((resA) => {
   console.log(`workA : ${resA}`);
-  workB(resA, (resB) => {
+  workB(resA).then((resB) => {
     console.log(`workB : ${resB}`);
-    workC(resB, (resC) => {
+    workC(resB).then((resC) => {
       console.log(`workC : ${resC}`);
     });
   });
 });
-// 이렇게 작성하면 여러 작업을 비동기로 처리하더라도 순서대로 처리할 수 있다.
-// 비동기 함수의 특정 결과값을 다른 비동기  함수에서 활용할 수 있다.
