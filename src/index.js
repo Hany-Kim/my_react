6cb667cee0f3f7dcc6199b26832ea3bfa72b6ffa
Hany-Kim <hanykim1210@gmail.com>
2025-11-10 20:54:02 +0900

    async와 await 4 - 총정리 & Promise All

--- src/index.js
+++ src/index.js
@@ -1,24 +1,51 @@
-// async와 await
-// 프로미스 객체를 더욱 쉽게 사용할 수 있도록 도와줌
-
-const delay = (ms) => {
+const workA = () => {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve("3초가 지났습니다.");
-    }, ms);
+      resolve("workA");
+    }, 5000);
   });
 };
 
-// async와 await 사용
-// async는 비동기를 사용할 함수의 오른쪽에 작성
-// async를 사용하면 해당 함수는 항상 Promise객체를 반환
-// await은 async함수를 작성한 내부에서 사용
+const workB = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("workB");
+    }, 3000);
+  });
+};
+
+const workC = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("workC");
+    }, 10000);
+  });
+};
+
+// 18초가 지나야 결과가 나옴
+// const start = async () => {
+//   try {
+//     const resultA = await workA();
+//     const resultB = await workB();
+//     const resultC = await workC();
+//     console.log(resultA);
+//     console.log(resultB);
+//     console.log(resultC);
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
+// Promise all 각 작업을 병렬적으로 처리해 작업시간 단축
+// 10초만에 끝남
+// 모든 프로미스가 작업이 완료되고 결과 한번에 반환
+// 하나라도 실패하면 실패, 모두 성공해야 성공
 const start = async () => {
   try {
-    let result = await delay(3000);
-    console.log(result);
-  } catch (error) {
-    console.log(error);
+    const result = await Promise.all([workA(), workB(), workC()]);
+    result.forEach((result) => console.log(result));
+  } catch (e) {
+    console.log(e);
   }
 };
 
