f2fc796597795dcac62bfdd67cd19b59369e4320
Hany-Kim <hanykim1210@gmail.com>
2025-11-18 00:37:43 +0900

    DOM 7 - textContent

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-let $color = document.getElementById("color");
+let $age = document.getElementById("age");
+$age.textContent = "5살";
 
-console.log($color.classList);
-// classList : class name처럼 특정 요소의 클래스 속성에 접근가능한 프로퍼티면서, 여러 메서드(add, remove, ...)를 제공
-
-$color.classList.add("dog-color");
-console.log($color); // <div class="info-item dog-color" id="color">갈색</div>
-// .add : 값을 변경하는게 아니라 전달한 요소를 기존 요소에 추가
-
-$color.classList.remove("info-item");
-console.log($color); // <div class="dog-color" id="color">갈색</div>
-// .remove : 제거
+console.log($age); // <div class=​"info-item" id=​"age">​5살​</div>​
