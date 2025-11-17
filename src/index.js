ca903eed2ecda9780cd187019530c20550f8a386
Hany-Kim <hanykim1210@gmail.com>
2025-11-18 00:28:57 +0900

    DOM 4 - 제어_class명 변경

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-// querySelectorAll : 전달받은 class이름을 가진 여러 요소를 찾아 반환
-// getElementsByClassName : class이름만으로 여러 요소를 반환
-// getElementsByTagName : 특정 Html 태그로 이루어진 모든 요소들을 반호나
+let $name = document.getElementById("name");
+$name.className = "dog-name";
 
-let $infoItem1 = document.querySelectorAll("div.info-item");
-console.log($infoItem1);
-
-let $infoItem2 = document.getElementsByClassName("info-item");
-console.log($infoItem2);
-
-let $infoItem3 = document.getElementsByTagName("div");
-console.log($infoItem3);
+console.log($name); // <div class="dog-name" id="name">강아지</div>
+console.log($name.className); // index.js:5 dog-name
