49d5af4298967be3b3abbb26d4f0d529fbed439a
Hany-Kim <hanykim1210@gmail.com>
2025-11-17 23:35:53 +0900

    DOM 1 - DOM API getElementById

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-// API
+// getElementById
 
-const getData = async () => {
-  try {
-    let response = await fetch("https://jsonplaceholder.typicode.com/users");
-    let data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-getData();
+// DOM API를 사용해서 특정 요소를 변수에 할당할 때, 명명규칙
+// $+변수명 or 변수명+Element
+let $color = document.getElementById("color");
+console.log($color); // <div id="color">갈색</div>
