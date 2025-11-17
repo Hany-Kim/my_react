93ba3ddf9093de2dafe0bf65c2a1ce1d81428d17
Hany-Kim <hanykim1210@gmail.com>
2025-11-17 23:40:47 +0900

    DOM 2 - DOM API querySelector

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-// getElementById
+// querySelector : getElementById처럼 단 하나 요소 반환
 
-// DOM API를 사용해서 특정 요소를 변수에 할당할 때, 명명규칙
-// $+변수명 or 변수명+Element
-let $color = document.getElementById("color");
-console.log($color); // <div id="color">갈색</div>
+let $animalInfo = document.querySelector("div.animal-info"); // 요소들 중 class명을 통해 특정요소를 찾을 때 -> .
+let ageElement = document.querySelector("div#age"); // 요소들 중 id명을 통해 특정요소를 찾을 때 -> #
+console.log($animalInfo);
+// <div class="animal-info">
+//   <div id="name">강아지</div>
+//   <div id="color">갈색</div>
+//   <div id="age">2살</div>
+// </div>
+console.log(ageElement); // <div id="age">2살</div>
