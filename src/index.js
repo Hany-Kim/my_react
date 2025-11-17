bdba9a83c474116049b608edf6bab9aff9202dbc
Hany-Kim <hanykim1210@gmail.com>
2025-11-18 00:36:37 +0900

    DOM 6 - classList

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-let $animalInfo = document.querySelector("div.animal-info");
-$animalInfo.id = "animal";
+let $color = document.getElementById("color");
 
-console.log($animalInfo);
-{
-  /* <div class="animal-info" id="animal">
-  <div class="info-item" id="name">강아지</div>
-  <div class="info-item" id="color">갈색</div>
-  <div class="info-item" id="age">2살</div>
-</div> */
-}
-console.log($animalInfo.id); // animal
+console.log($color.classList);
+// classList : class name처럼 특정 요소의 클래스 속성에 접근가능한 프로퍼티면서, 여러 메서드(add, remove, ...)를 제공
+
+$color.classList.add("dog-color");
+console.log($color); // <div class="info-item dog-color" id="color">갈색</div>
+// .add : 값을 변경하는게 아니라 전달한 요소를 기존 요소에 추가
+
+$color.classList.remove("info-item");
+console.log($color); // <div class="dog-color" id="color">갈색</div>
+// .remove : 제거
