3f9393548a5bacd1e861a645023d2f6dc696c755
Hany-Kim <hanykim1210@gmail.com>
2025-11-18 23:18:41 +0900

    DOM 9 - createElement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-let $color = document.getElementById("color");
-$color.style.color = "blue";
-$color.style.fontSize = "30px";
+// createElement : 새로운 요소 생성
 
-console.log($color); // <div class="info-item" id="color" style="color: blue; font-size: 30px;">갈색</div>
+let $type = document.createElement("div");
+$type.className = "info-item";
+$type.id = "type";
+
+// 방법 1
+// $type.textContent = "말티즈";
+// console.log($type); // <div class=​"info-item" id=​"type">​말티즈​</div>​
+
+// 방법 2
+let $typeText = document.createTextNode("말티즈");
+console.log($type); // <div class=​"info-item" id=​"type"></div>​
+console.log($typeText); // 말티즈
+
+// appendChild : 요소 추가
+let $animalInfo = document.querySelector("div.animal-info");
+$animalInfo.appendChild($type);
+$type.appendChild($typeText);
+console.log($type); // <div class=​"info-item" id=​"type">​말티즈​</div>
